814aace7556e2982d1fa4dd4457c913167f76934
bolong.wang@hellofresh.com.au <bolong.wang@hellofresh.com.au>
2025-06-05 13:23:16 +1000

    feat: add safe metadat

--- lua/jupytext/utils.lua
+++ lua/jupytext/utils.lua
@@ -14,6 +14,14 @@ local language_names = {
 
 M.get_ipynb_metadata = function(filename)
   local metadata = vim.json.decode(io.open(filename, "r"):read "a")["metadata"]
+  if not metadata then
+    metadata = {
+      nbformat = 4,
+      nbformat_minor = 5,
+      metadata = {},
+      cells = {},
+    }
+  end
   local kernelspec = (metadata and metadata.kernelspec)
     or {
       name = "python3",
@@ -29,11 +37,6 @@ M.get_ipynb_metadata = function(filename)
 end
 
 M.get_jupytext_file = function(filename, extension)
-  -- local jupytext = metadata and metadata.jupytext
-  -- if not jupytext or type(jupytext.formats) ~= "string" then
-  --   return ".md"
-  -- end
-
   local fileroot = vim.fn.fnamemodify(filename, ":r")
   return fileroot .. "." .. extension
 end
